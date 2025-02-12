ca05c3f72037e86efcc733bf45280afcf68857f1
CORE Technologies <112516594+Uwami-Mgxekwa@users.noreply.github.com>
2025-02-12 19:59:04 +0200

    Add files via upload

---
+++ Landing Page/script.js
@@ -0,0 +1,24 @@
+function redirectToLogin() {
+    alert("Login page is under construction. Check back soon!");
+    window.location.href = "login.html";
+  }
+  
+  function redirectToSignup() {
+    alert("Signup page is under construction. Check back soon!");
+    // window.location.href = "https://example.com/signup"; 
+  }
+  
+  function redirectToSupport() {
+    alert("Support page is under construction. Check back soon!");
+    // window.location.href = "https://example.com/support"; 
+  }
+  
+  function redirectToFAQ() {
+    alert("FAQ page is under construction. Check back soon!");
+    // window.location.href = "https://example.com/faq"; 
+  }
+  
+  function redirectToWebsite() {
+    alert("Main website is under construction. Check back soon!");
+    // window.location.href = "https://example.com"; 
+  }
