d87b1fd6635ea757304eaf4b11ace8582aced597
Misterh100 <handsomenyathi1@gmail.com>
2025-02-19 20:20:12 +0200

    refactor: Login,register, styles

--- js/login-script.js
+++ js/login-script.js
@@ -1,14 +1,23 @@
-//Constants
-const name = document.getElementById("name");
-const email = document.getElementById("email");
+import postData from "../lib/post-data.js";
+import checkFormValidity from "../lib/validate-form.js";
+const loginForm = document.getElementById("login-form");
 const password = document.getElementById("password");
-const wrapper = document.querySelector(".wrapper"),
-  signupHeader = document.querySelector(".signup header"),
-  loginHeader = document.querySelector(".login header");
+const studentID = document.getElementById("id");
+const loginBtn = document.getElementById("login-btn");
+const loginUrl = "https://sp-server-ts.onrender.com/api/login";
+//const loginUrl = "http://localhost:6969/api/login";
 
-loginHeader.addEventListener("click", () => {
-  wrapper.classList.add("active");
-});
-signupHeader.addEventListener("click", () => {
-  wrapper.classList.remove("active");
+const login = () => {
+  let data = {
+    studentID: studentID.value,
+    password: password.value,
+  };
+  postData(loginUrl, data);
+};
+
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (checkFormValidity(loginForm)) {
+    login();
+  }
 });
